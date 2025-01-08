d44f9e10a002e55cbd9db946de019d6249bf7609
Harsh <vishwaharsh10@gmail.com>
2025-01-08 23:48:29 +0530

    First nodejs Example

---
+++ hello-world-node/index.js
@@ -0,0 +1,9 @@
+console.log("Hello this is node js file")
+
+const arr = [1,2,3,4,5]
+
+setTimeout(()=> {
+    console.log("This message will be delayed by 4 seconds")
+}, 4000);
+
+console.log("Hello world my name is harsh vishwakarma")
